9cd7b5da06c7ad3089218e59c4c5c626be3885df
Will <yzj_000@163.com>
2026-01-13 21:32:06 +0800

    Refactor: extract a method for current main logic

--- app/streaming_different_approaches.py
+++ app/streaming_different_approaches.py
@@ -14,59 +14,59 @@ base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 # api_key=os.getenv("DEEPSEEK_API_KEY")
 # base_url="https://api.deepseek.cn/v1"
 
-joke_model = init_chat_model(
-    model=model,
-    api_key=api_key,
-    base_url=base_url,
-    model_provider="openai",
-    tags=["joke"]
-)
-poem_model = init_chat_model(
-    model=model,
-    api_key=api_key,
-    base_url=base_url,
-    model_provider="openai",
-    tags=["poem"]
-)
-
-class State(TypedDict):
-    topic: str
-    joke: str
-    poem: str
-
-async def call_model(state: State, config) -> State:
-    topic = state["topic"]
-    print("Writing joke...")
-    joke_response = await joke_model.ainvoke(
-        [{"role": "user", "content": f"Write a joke about {topic}"}],
-        config,
-    )
-
-    print("Writing poem...")
-    poem_response = await poem_model.ainvoke(
-        [{"role": "user", "content": f"Write a short poem about {topic}"}],
-        config,
-    )
-    
-    return {
-        "joke": joke_response.content,
-        "poem": poem_response.content,
-    }
-
-graph = (
-    StateGraph(State)
-    .add_node(call_model)
-    .add_edge(START, "call_model")
-    .add_edge("call_model", END)
-    .compile()
-)
-
 async def filter_by_tags():
+    joke_model = init_chat_model(
+        model=model,
+        api_key=api_key,
+        base_url=base_url,
+        model_provider="openai",
+        tags=["joke"]
+    )
+    poem_model = init_chat_model(
+        model=model,
+        api_key=api_key,
+        base_url=base_url,
+        model_provider="openai",
+        tags=["poem"]
+    )
+
+    class State(TypedDict):
+        topic: str
+        joke: str
+        poem: str
+
+    async def call_model_fn(state: State, config) -> State:
+        topic = state["topic"]
+        print("\nWriting joke...\n")
+        joke_response = await joke_model.ainvoke(
+            [{"role": "user", "content": f"Write a joke about {topic}"}],
+            config,
+        )
+
+        print("\nWriting poem...\n")
+        poem_response = await poem_model.ainvoke(
+            [{"role": "user", "content": f"Write a short poem about {topic}"}],
+            config,
+        )
+        
+        return {
+            "joke": joke_response.content,
+            "poem": poem_response.content,
+        }
+
+    graph = (
+        StateGraph(State)
+        .add_node("call_model", call_model_fn)
+        .add_edge(START, "call_model")
+        .add_edge("call_model", END)
+        .compile()
+    )
+
     async for msg, metadata in graph.astream(
         {"topic": "cats"},
         stream_mode="messages",
     ):
-        if metadata["tags"] == ["joke"]:
+        if metadata["tags"] == ["poem"]:
             print(msg.content, end="", flush=True)
 
 if __name__ == "__main__":
